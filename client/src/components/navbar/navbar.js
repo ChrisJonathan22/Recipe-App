e7924fff680a92cd27aa32b44dd9a1c7e68da840
Chris Jonathan <christophereko@hotmail.fr>
2018-12-21 09:32:07 +0000

    I have switched from Materialize css to Bootswatch, the menu now works as intended though the links appeared to have the same colour as the nav background so I had to change the nav link colour manually to white. I need to work on the 2 remaining pages.

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -10,17 +10,27 @@ class Navbar extends Component {
   render() {
     return (
             <div>
-                <nav className = 'nav-wrapper red darken-3'>
-                  <div className = 'container'>
-                    {/* Using Link enables us to stop the app from making a request to the server every time we click on a link to take us to a different component and NavLink does the same except when you click a link the link clicked on receives a class of active.  */}
-                      <a className = 'brand-logo left'><NavLink exact to = '/'>Logo</NavLink></a>
-                      <ul id = 'nav-mobile' className = 'right hide-on-med-and-down'>
-                          <li><a><NavLink exact to = '/'>Home</NavLink></a></li>
-                          <li><a><NavLink to = '/recipes'>Recipes</NavLink></a></li>
-                          <li><a><NavLink to = '/about'>About</NavLink></a></li>
-                          <li><a><NavLink to = '/contact'>Contact</NavLink></a></li>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                  <a className="navbar-brand"><NavLink exact to = '/'>Logo</NavLink></a>
+                    <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                      <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="navbar-collapse collapse" id="navbarColor01">
+                      <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                          <a className="nav-link"><NavLink exact to = '/'>Home</NavLink><span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link"><NavLink to = '/recipes'>Recipes</NavLink></a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link"><NavLink to = '/about'>About</NavLink></a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link"><NavLink to = '/contact'>Contact</NavLink></a>
+                        </li>
                       </ul>
-                  </div>
+                    </div>
                 </nav>
             </div>
     );
